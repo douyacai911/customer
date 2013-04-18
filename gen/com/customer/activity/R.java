bd0fe3060bc1313ba355811033070359a91c9f04
Tang Xiaodong <douyacai911@gmail.com>
2013-04-18 19:40:09 +0800

    加入餐厅列表，按距离排序

--- gen/com/customer/activity/R.java
+++ gen/com/customer/activity/R.java
@@ -142,34 +142,41 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f040015;
-        public static final int button1=0x7f040011;
-        public static final int editText1=0x7f040008;
-        public static final int editText2=0x7f040006;
-        public static final int emailText=0x7f040013;
-        public static final int fragment1=0x7f040014;
+        public static final int Button1=0x7f040006;
+        public static final int Distance=0x7f04001a;
+        public static final int ListView01=0x7f040007;
+        public static final int RelativeLayout01=0x7f040018;
+        public static final int RestName=0x7f040019;
+        public static final int action_settings=0x7f04001b;
+        public static final int button1=0x7f040014;
+        public static final int editText1=0x7f04000b;
+        public static final int editText2=0x7f040009;
+        public static final int emailText=0x7f040016;
+        public static final int fragment1=0x7f040017;
         public static final int hybrid=0x7f040004;
-        public static final int loginBtn=0x7f04000a;
+        public static final int loginBtn=0x7f04000d;
         public static final int none=0x7f040000;
         public static final int normal=0x7f040001;
-        public static final int progressBar1=0x7f040010;
-        public static final int pwdText=0x7f04000d;
-        public static final int pwdText2=0x7f04000f;
-        public static final int registerBtn=0x7f04000b;
+        public static final int progressBar1=0x7f040013;
+        public static final int pwdText=0x7f040010;
+        public static final int pwdText2=0x7f040012;
+        public static final int registerBtn=0x7f04000e;
         public static final int satellite=0x7f040002;
         public static final int terrain=0x7f040003;
-        public static final int textView1=0x7f040005;
-        public static final int textView2=0x7f040007;
-        public static final int textView3=0x7f04000c;
-        public static final int textView4=0x7f040012;
-        public static final int userText=0x7f04000e;
-        public static final int waitingBar=0x7f040009;
+        public static final int test1=0x7f040005;
+        public static final int textView1=0x7f040008;
+        public static final int textView2=0x7f04000a;
+        public static final int textView3=0x7f04000f;
+        public static final int textView4=0x7f040015;
+        public static final int userText=0x7f040011;
+        public static final int waitingBar=0x7f04000c;
     }
     public static final class layout {
         public static final int activity_begin=0x7f030000;
         public static final int activity_login=0x7f030001;
         public static final int activity_register=0x7f030002;
         public static final int activity_test=0x7f030003;
+        public static final int rest_list_layout=0x7f030004;
     }
     public static final class menu {
         public static final int begin=0x7f080000;
--- src/com/customer/activity/BeginActivity.java
+++ src/com/customer/activity/BeginActivity.java
@@ -1,30 +1,161 @@
 package com.customer.activity;
 
-import com.customer.activity.R;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.customer.activity.R;
+import com.customer.util.HttpUtil;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BeginActivity extends Activity {
-	
+	private int customerid = 0;
 	private TextView textview;   //TODO test
-	
+	private Handler mainHandler;
+	private SimpleAdapter listItemAdapter;
+	private ListView list;
+	private double mylon = 0;
+	private double mylat = 0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_begin);
-		
-		textview = (TextView) findViewById(R.id.textView1);
+		list = (ListView) findViewById(R.id.ListView01);
+		textview = (TextView) findViewById(R.id.test1);
 		Intent intent = this.getIntent();
 		Bundle bundle = intent.getExtras();
-		int customerid = bundle.getInt("customerid");
+		customerid = bundle.getInt("customerid");
 		String id = String.valueOf(customerid);
 		textview.setText(id);
+		
+		LocationManager loctionManager;
+		String contextService=Context.LOCATION_SERVICE;
+		//通过系统服务，取得LocationManager对象
+		loctionManager=(LocationManager) getSystemService(contextService);
+		
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);//高精度
+		criteria.setAltitudeRequired(false);//不要求海拔
+		criteria.setBearingRequired(false);//不要求方位
+		criteria.setCostAllowed(true);//允许有花费
+		criteria.setPowerRequirement(Criteria.POWER_LOW);//低功耗
+		//从可用的位置提供器中，匹配以上标准的最佳提供器
+		String provider = loctionManager.getBestProvider(criteria, true);
+		//获得最后一次变化的位置
+		Location location = loctionManager.getLastKnownLocation(provider);
+		mylon = location.getLongitude();
+		mylat = location.getLatitude();
+		
+		
+		
+		
+		new Thread(progressThread).start();
+		
+		this.mainHandler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				// findViewById(R.id.progressBar1).setVisibility(View.GONE);
+				if(msg.what==-123){
+					Toast.makeText(BeginActivity.this, "尚未有餐厅注册",Toast.LENGTH_SHORT).show();
+				}else{
+				listItemAdapter = new SimpleAdapter(BeginActivity.this,
+						(ArrayList<HashMap<String, Object>>) msg.obj,// 数据源
+						R.layout.rest_list_layout,// ListItem的XML实现
+						// 动态数组与ImageItem对应的子项
+						new String[] { "restname", "distance" },//TODO
+						// ImageItem的XML文件里面的一个ImageView,两个TextView ID
+						new int[] { R.id.RestName, R.id.Distance }
+
+				);
+
+				// 添加并且显示
+				list.setAdapter(listItemAdapter);
+				}
+
+				super.handleMessage(msg);
+			}
+
+		};
+	}
+	Runnable progressThread = new Runnable() {
+		@Override
+		public void run() {
+			Message msg = new Message();
+			new Thread();
+			Object flag = (Object)jsonToRest(mylon,mylat,customerid);
+			
+			if(flag==null){
+				msg.what = -123;
+			}else{
+				msg.obj = flag;
+			}
+			mainHandler.sendMessage(msg);
+		}
+	};
+
+	private String goSearchRest(double lon, double lat, int customerid) {
+		// 查询参数
+
+		String registerString = "customerid=" + customerid + "&lon=" + lon + "&lat=" + lat;
+		// URL
+		String url = HttpUtil.BASE_URL + "RestListServlet?" + registerString;
+		// 查询返回结果
+		return HttpUtil.queryStringForPost(url);
 	}
 
+	private ArrayList<HashMap<String, Object>> jsonToRest(double lon,double lat,int customerid) {
+		String jsonString = goSearchRest(lon,lat,customerid);
+		if(jsonString.equals("-1")){
+			return null;
+		}
+
+		ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+		try {
+			JSONObject json = new JSONObject(jsonString);
+			JSONArray jsonArray = json.getJSONArray("restlist");
+
+			for (int i = 0; i < jsonArray.length(); i++) {
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				JSONObject rest = (JSONObject) jsonArray.get(i);
+				double distance = rest.getDouble("distance");
+				String restname = rest.getString("restname");
+				int restid = rest.getInt("restid");
+				map.put("distance", distance);
+				map.put("restname", restname);
+				map.put("restid", restid);
+				map.put("index", i);
+				listItem.add(map);
+			}
+			return listItem;
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Log.i(jsonString, "????");
+			return null;
+		}
+
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
